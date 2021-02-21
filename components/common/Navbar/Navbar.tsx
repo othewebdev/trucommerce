eafd954bb4a420991882386878222b9e2af88900
srcrendevgit <69765129+srcrendevgit@users.noreply.github.com>
2021-02-21 12:41:50 -0600

    Navbar fixes x2

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -33,7 +33,7 @@ const Navbar: FC = () => {
                 <Logo />
               </a>
             </Link>
-            <nav className="w-200 ml-4 space-x-2 sm:block lg:block">
+            <nav className="w-200 ml-4 space-x-2 block">
               <Link href="/search">
                 <a className={s.link}>Products</a>
               </Link>
