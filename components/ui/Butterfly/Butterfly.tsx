13ef656b4ef03691e639193897fd48da3cb6fdd0
srcrendevgit <69765129+srcrendevgit@users.noreply.github.com>
2021-05-05 16:38:52 -0500

    Header changes

--- components/ui/Butterfly/Butterfly.tsx
+++ components/ui/Butterfly/Butterfly.tsx
@@ -1,3 +1,8 @@
-const Butterfly = () => <img style={{}} src="../../../icon-512x1024.png"></img>
+const Butterfly = () => (
+  <img
+    style={{ maxWidth: '550px', width: '100%' }}
+    src="../../../icon-512x1024.png"
+  ></img>
+)
 
 export default Butterfly
--- components/ui/Hero/Hero.tsx
+++ components/ui/Hero/Hero.tsx
@@ -15,7 +15,7 @@ const Hero: FC<Props> = ({ headline, description, bg, text }) => {
   return (
     <div className={bg}>
       <Container>
-        <div style={{ display: 'grid', gridTemplateColumns: '1fr 2fr' }}>
+        <div className="gridContainer">
           <div
             style={{
               display: 'flex',
@@ -25,18 +25,17 @@ const Hero: FC<Props> = ({ headline, description, bg, text }) => {
           >
             <Butterfly />
           </div>
-
           <div style={{ textAlign: 'left' }} className={s.root}>
             <h2
               style={{ textTransform: 'capitalize', lineHeight: '3.5rem' }}
-              className={`md:text-6xl lg:text-6xl text-4xl text-center font-bold   ${text}`}
+              className={`text-6xl text-center font-bold   ${text}`}
             >
               {headline}
             </h2>
             <div className="flex flex-col justify-between">
               <h2
                 style={{ textTransform: 'capitalize', lineHeight: '2rem' }}
-                className={`text-sm md:text-lg lg:text-lg font-medium text-center ${text}`}
+                className={`text-lg font-medium text-center ${text}`}
               >
                 {description}
               </h2>
