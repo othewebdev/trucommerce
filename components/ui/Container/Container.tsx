967f7af7133ca3171826b1b0e65d5925f09ca69e
obarnettdev01 <obarnettdev01@gmail.com>
2021-05-30 11:40:20 -0500

    Better margin + button tweaks

--- components/ui/Container/Container.tsx
+++ components/ui/Container/Container.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const Container: FC<Props> = ({ children, className, el = 'div', clean }) => {
   const rootClassName = cn(className, {
-    'max-w-8xl sm:mx-4 md:mx-8 lg:mx-36': !clean,
+    'max-w-8xl sm:mx-4 md:mx-8 lg:mx-12': !clean,
   })
 
   let Component: React.ComponentType<
